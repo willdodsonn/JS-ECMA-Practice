cd393f551286b652431353e782f3bb6116a2dde5
willdodsonn <sodlax38@gmail.com>
2021-12-28 20:54:22 +0000

    sixth commit

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,7 +1,26 @@
 function fizzBuzz() {  
 	// Your code here
-
+	for (var i = 1; i < 101; i++) {
+        
+		if (i % 5 == 0 && i % 3 == 0){
+			console.log('FizzBuzz')
+		}
+		else if (i % 3 == 0){
+			console.log('fizz');
+		}
+		else if (i % 5 == 0){
+			console.log('Buzz')
+		}
+		else {
+			console.log(i)	
+		}
+		
+	}
 	
+
+
 }
 
 fizzBuzz();
+
+
--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -2,4 +2,4 @@
 
 //Your code above ^^^
 
-console.log(sing());
+console.log('let it be, let it be, let it be, let it be, whisper words of wisdom, let it be, let it be, let it be, let it be, let it be, there will be an answer, let it be');

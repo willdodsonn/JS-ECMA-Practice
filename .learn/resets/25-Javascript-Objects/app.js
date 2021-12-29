dfb4b81e0d45616c1a5b00577813124ce3071207
willdodsonn <sodlax38@gmail.com>
2021-12-29 23:46:58 +0000

    sixth commit

--- .learn/resets/25-Javascript-Objects/app.js
+++ .learn/resets/25-Javascript-Objects/app.js
@@ -16,33 +16,15 @@ var person2 = {
     significant_other: person
 };
 
-var person3 = {
-    name: "Jimmy",
-    lastname: "Doe",
-    age: 13,
-    gender: "male",
-    lucky_numbers: [ 1, 2, 3, 4],
-    significant_other: null
-    
-};
-
-
-
-
 var family = {
     lastname: "Doe",
-    members: [person, person2, person3]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2]   //Array of objects, don't forget to add Jimmy
 };
 
 
-function addAllFamilyLuckyNumbers(anArray) {
+function addAllFamilyLuckyNumbers(anArray){
   var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-    
-  var anArray = sumOfAllLuckyNumbers.length;
-  for (var i = 0; i < arrayLength; i++) {
-      console.log(anArray[i]);
-      
-  }
+
   //To-Do: loop and add; consider nested loops
   //Hint: use the anArray variable to get all of the lucky numbers
   
@@ -50,7 +32,6 @@ function addAllFamilyLuckyNumbers(anArray) {
 }
 
 //Enter all your code here:
-family.members[0].significant_other.lucky_numbers[3] = 33;
 
 
 //Do not make changes below:
--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,28 +1,35 @@
-// Your code here:
-const bottles = "bottles";
-const bottle = "bottle";
-const onTheWall = " of milk on the wall";
-const noBottles= "No bottles of milk on the wall"; 
-const takeOne = "Take one down, pass it around,";
-const goToStore = "go to the store and by some more.";
+const bottles = " bottles";
+const bottle = " bottle";
+const onTheWall = " of milk on the wall, ";
+const noBottles= " No more bottles of milk on the wall, "; 
+const takeOne = " Take one down and pass it around, ";
+const goToStore = " Go to the store and buy some more,";
 
 
 function bottlesOfMilk() {  
     
     for (var i = 99; i >= 0; i--) {
-        if (i == 0){
-            console.log(0);
+        if (i == 2) {
+            console.log( i + bottles + onTheWall + i + bottles + ' of milk. ' + takeOne + ' 1 bottle of milk on the wall.');
         }
-        else if (i == 1) {
-            console.log(1);
-        } 
+        else if (i == 1){
+           console.log ('1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.');
+            //console.log( i + bottle + onTheWall + i + bottle + ' of milk. ' + takeOne + ' no more bottles of milk on the wall.');
+        }
+        else if (i == 0){
+           console.log( 'No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.');
+            //console.log( noBottles + ' no more bottles of milk. ' + goToStore + ' 99 bottles of milk on the wall. ' );
+        }
+        
         else {
-            console.log(i + bottles + onTheWall + i + bottles + 'of milk.' + takeOne + (i - 1) + bottles + onTheWall
+            console.log( i + bottles + onTheWall + i + bottles + ' of milk.' + takeOne + (i - 1) + bottles + ' of milk on the wall. '
             );
             //console.log(i + bottles + onTheWall + i + bottles + 'of milk.' + takeOne + i - 1 + bottles + onTheWall);
         } 
      }
 }   
 
-
 bottlesOfMilk();
+
+
+
